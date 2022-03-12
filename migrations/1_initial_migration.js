822b2bca5231578a776ebf101d9fe58f542a9479
paulino-s <paulinovasquez900@gmail.com>
2022-03-11 19:40:32 -0600

    add contract and test

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const ListTodo = artifacts.require("ListTodo");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(ListTodo);
 };
